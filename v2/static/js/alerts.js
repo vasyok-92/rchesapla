0bbdb895d9c2f3997fc03eeb710b23646c6d4f10
Fatih ÇELİK <fatihcelikofficialtr@gmail.com>
2022-01-12 21:58:35 +0300

    xx

--- v2/static/js/alerts.js
+++
@@ -1,9 +0,0 @@
-alertDismiss = function(cate){
-    if (cate == 'error') {
-        var alert = document.getElementById('errorAlert');
-        alert.style.display = "none";
-    } else {
-        var alert = document.getElementById('successAlert');
-        alert.style.display = "none";
-    }
-}
--- v2/static/js/inputPost.js
+++
@@ -1,9 +0,0 @@
-function getData(field) {
-    var field2 = document.getElementById(field).value
-
-    fetch('/input_data/'+field, {
-        method: 'POST',
-        body: JSON.stringify({ [field]: field2 })
-    }).then(response => {
-    });
-}
