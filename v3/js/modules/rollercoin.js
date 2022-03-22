3b33d141a8bb2965c32e2ba7e2dfe5fb3897f865
Fatih ÇELİK <fatihcelikofficialtr@gmail.com>
2022-03-22 03:20:53 +0300

    Güncellendi!

--- v3/js/modules/rollercoin.js
+++ v3/js/modules/rollercoin.js
@@ -14,14 +14,14 @@ export const blockReward = {
         icon: "doge.svg"
     },
     "ETH": {
-        dailyReward: 0.008,
+        dailyReward: 0.005,
         timePerBlock: 600,
         name: "Ethereum",
         ticker: "ETH",
         icon: "eth.svg"
     },
     "BNB": {
-        dailyReward: 0.02,
+        dailyReward: 0.012,
         timePerBlock: 600,
         name: "Binancecoin",
         ticker: "BNB",
