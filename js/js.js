08ed72e1d1d7b6dedeb29676a291ba055a42aa2e
Fatih ÇELİK <fatihcelikofficialtr@gmail.com>
2022-05-09 22:56:36 +0300

    Güncellendi!

--- js/js.js
+++ js/js.js
@@ -8,13 +8,13 @@ function blockAmount() {
     switch (document.getElementById("block-type").selectedIndex) {
         case 0:
             //Bitcoin - 30000
-            var blockreward = 30000;
-            document.getElementById("block-reward").value = 30000;
+            var blockreward = 45000;
+            document.getElementById("block-reward").value = 45000;
             break;
         case 1:
             //Doge - 20
-            var blockreward = 20;
-            document.getElementById("block-reward").value = 20;
+            var blockreward = 40;
+            document.getElementById("block-reward").value = 40;
             break;
         case 2:
             //Eth - 0.005
@@ -23,13 +23,13 @@ function blockAmount() {
             break;
         case 3:
             //Bnb - 0.012
-            var blockreward = 0.018;
-            document.getElementById("block-reward").value = 0.018;
+            var blockreward = 0.012;
+            document.getElementById("block-reward").value = 0.012;
             break;
         case 4: 
             //Matic - 3
-            var blockreward = 5;
-            document.getElementById("block-reward").value = 5;
+            var blockreward = 3;
+            document.getElementById("block-reward").value = 3;
             console.log("matic end");
             break;
         case 5: 
@@ -41,8 +41,8 @@ function blockAmount() {
     default:
             //RLT - 30
             console.log("rlt start");
-            var blockreward = 40;
-            document.getElementById("block-reward").value = 40;
+            var blockreward = 30;
+            document.getElementById("block-reward").value = 30;
             break;
     }
 }
--- js/js.js.bak
+++ js/js.js.bak
@@ -18,31 +18,31 @@ function blockAmount() {
             break;
         case 2:
             //Eth - 0.005
-            var blockreward = 0.008;
-            document.getElementById("block-reward").value = 0.008;
+            var blockreward = 0.005;
+            document.getElementById("block-reward").value = 0.005;
             break;
         case 3:
             //Bnb - 0.012
-            var blockreward = 0.012;
-            document.getElementById("block-reward").value = 0.012;
+            var blockreward = 0.018;
+            document.getElementById("block-reward").value = 0.018;
             break;
         case 4: 
             //Matic - 3
-            var blockreward = 3;
-            document.getElementById("block-reward").value = 3;
+            var blockreward = 5;
+            document.getElementById("block-reward").value = 5;
             console.log("matic end");
             break;
         case 5: 
             //SOL - 0.05
-            var blockreward = 0.09;
-            document.getElementById("block-reward").value = 0.09;
+            var blockreward = 0.05;
+            document.getElementById("block-reward").value = 0.05;
             console.log("sol end");
             break;
     default:
             //RLT - 30
             console.log("rlt start");
-            var blockreward = 30;
-            document.getElementById("block-reward").value = 30;
+            var blockreward = 40;
+            document.getElementById("block-reward").value = 40;
             break;
     }
 }
