d4f1b4a6b40929ae0c99c7d13cc48183abdbdc75
Fatih ÇELİK <fatihcelikofficialtr@gmail.com>
2022-05-16 17:21:42 +0300

    Güncellendi!

--- v4/js/calculator.js
+++ v4/js/calculator.js
@@ -308,8 +308,8 @@ function blockAmount() {
   switch (document.getElementById("block-type").selectedIndex) {
     case 0:
       //BITCOIN BLOK ÖDÜLÜ > 30000
-      var blockreward = 45000;
-      document.getElementById("block-reward").value = 45000;
+      var blockreward = 30000;
+      document.getElementById("block-reward").value = 30000;
 
       document.getElementById("exp_reward").innerHTML = "Hesaplanmadı!";
       document.getElementById("daily").innerHTML = "Hesaplanmadı!";
@@ -324,8 +324,8 @@ function blockAmount() {
       break;
     case 1:
       //DOGE BLOK ÖDÜLÜ > 20
-      var blockreward = 40;
-      document.getElementById("block-reward").value = 40;
+      var blockreward = 20;
+      document.getElementById("block-reward").value = 20;
 
       document.getElementById("exp_reward").innerHTML = "Hesaplanmadı!";
       document.getElementById("daily").innerHTML = "Hesaplanmadı!";
@@ -338,8 +338,8 @@ function blockAmount() {
       break;
     case 2:
       //ETH BLOK ÖDÜLÜ > 0.005
-      var blockreward = 0.005;
-      document.getElementById("block-reward").value = 0.005;
+      var blockreward = 0.008;
+      document.getElementById("block-reward").value = 0.008;
 
       document.getElementById("exp_reward").innerHTML = "Hesaplanmadı!";
       document.getElementById("daily").innerHTML = "Hesaplanmadı!";
@@ -378,8 +378,8 @@ function blockAmount() {
       break;
 	case 5:
       //SOL BLOK ÖDÜLÜ > 0.05
-      var blockreward = 0.05;
-      document.getElementById("block-reward").value = 0.05;
+      var blockreward = 0.07;
+      document.getElementById("block-reward").value = 0.07;
 
       document.getElementById("exp_reward").innerHTML = "Hesaplanmadı!";
       document.getElementById("daily").innerHTML = "Hesaplanmadı!";
@@ -393,8 +393,8 @@ function blockAmount() {
     default:
       //RLT BLOK ÖDÜLÜ > 30
       console.log("rlt start");
-      var blockreward = 30;
-      document.getElementById("block-reward").value = 30;
+      var blockreward = 45;
+      document.getElementById("block-reward").value = 45;
 
       document.getElementById("exp_reward").innerHTML = "Hesaplanmadı!";
       document.getElementById("daily").innerHTML = "Hesaplanmadı!";
